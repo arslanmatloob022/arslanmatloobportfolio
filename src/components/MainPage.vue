d19fcd11777b7190001e9b2057a2da0243ed049f
Arslan Matloob <raoarslan263@gmail.com>
2023-11-10 23:32:18 +0500

    projects

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -20,11 +20,10 @@
         </div>
       </div>
     </div>
-
     <div class="main">
       <div class="education borders divs row">
         <div class="siders">
-          <i class="fa fa-book fa-2x" aria-hidden="true"></i>
+          <i class="fa fa-book"></i>
         </div>
         <h1 class="col-lg-12 col-md-12 col-sm-12 col-12">Education</h1>
         <div class="graduation col-lg-4 col-md-4 col-sm-4 col-4">
@@ -50,46 +49,73 @@
 
       <div class="skills borders divs row">
         <div class="siders">
-          <i class="fa fa-book fa-2x" aria-hidden="true"></i>
+          <i class="fa fa-cogs" aria-hidden="true"></i>
         </div>
         <h1 class="col-12">Skills</h1>
         <div class="graduation col-lg-6 col-md-6 col-sm-6 col-6">
           <h3>Technical Skills</h3>
-          <h6><i class="fa fa-dashcube" aria-hidden="true"></i> Vue.Js</h6>
-          <h6><i class="fa fa-dashcube" aria-hidden="true"></i> Mongodb</h6>
-          <h6><i class="fa fa-dashcube" aria-hidden="true"></i> JavaScript</h6>
-          <h6><i class="fa fa-dashcube" aria-hidden="true"></i> Bootstrap</h6>
-          <h6><i class="fa fa-dashcube" aria-hidden="true"></i> HTML</h6>
-          <h6><i class="fa fa-dashcube" aria-hidden="true"></i> CSS</h6>
+          <h6><i class="fa fa-cube" aria-hidden="true"></i> Vue.Js</h6>
+          <h6><i class="fa fa-cube" aria-hidden="true"></i> Mongodb</h6>
+          <h6><i class="fa fa-cube" aria-hidden="true"></i> JavaScript</h6>
+          <h6><i class="fa fa-cube" aria-hidden="true"></i> Bootstrap</h6>
+          <h6><i class="fa fa-cube" aria-hidden="true"></i> HTML</h6>
+          <h6><i class="fa fa-cube" aria-hidden="true"></i> CSS</h6>
         </div>
         <div class="graduation col-lg-6 col-md-6 col-sm-6 col-6">
           <h3>Non-Technical Skills</h3>
           <h6>
-            <i class="fa fa-dashcube" aria-hidden="true"></i> Continous Learning
+            <i class="fa fa-cube" aria-hidden="true"></i> Continous Learning
           </h6>
           <h6>
-            <i class="fa fa-dashcube" aria-hidden="true"></i> Supportive for
-            others
+            <i class="fa fa-cube" aria-hidden="true"></i> Supportive for others
           </h6>
-          <h6><i class="fa fa-dashcube" aria-hidden="true"></i> Team Player</h6>
-          <h6><i class="fa fa-dashcube" aria-hidden="true"></i> Punctual</h6>
-          <h6><i class="fa fa-dashcube" aria-hidden="true"></i> Hardworking</h6>
+          <h6><i class="fa fa-cube" aria-hidden="true"></i> Team Player</h6>
+          <h6><i class="fa fa-cube" aria-hidden="true"></i> Punctual</h6>
+          <h6><i class="fa fa-cube" aria-hidden="true"></i> Hardworking</h6>
         </div>
       </div>
+
       <div class="skills borders divs row">
         <div class="siders">
-          <i class="fa fa-bookmark fa-2x" aria-hidden="true"></i>
+          <i class="fa fa-id-badge" aria-hidden="true"></i>
         </div>
         <h1 class="col-12">Experiance</h1>
         <h3 class="col-12">Work Experiance</h3>
         <h6 class="col-12">
-          <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>
+          <i class="fa fa-cube" aria-hidden="true"></i>
           Upto 1-Year experiance in fornt-end web development.
         </h6>
         <h6 class="col-12">
-          <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>
+          <i class="fa fa-cube" aria-hidden="true"></i>
           Currently working on Fiverr with friends with a team of three members.
         </h6>
+      </div>
+
+      <div class="skills borders divs row">
+        <div class="siders">
+          <i class="fas fa-folder-open"></i>
+        </div>
+        <h1 class="col-lg-12 col-md-12 col-sm-12 col-12">Projects</h1>
+        <div class="projects col-lg-12 col-md-12 col-sm-12 col-12">
+          <div class="pro-main row">
+            <div
+              v-for="project in projects"
+              :key="project.id"
+              class="proj-card col-lg-3 col-md-3 col-sm-4 col-4"
+            >
+              <div class="images">
+                <img :src="project.image" alt="" />
+              </div>
+              <div class="info">
+                <h4>{{ project.name }}</h4>
+                <p>{{ project.description }}</p>
+              </div>
+              <a :href="project.link" target="_blank" rel="noopener noreferrer">
+                <button class="view-btn">View</button></a
+              >
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -97,6 +123,44 @@
 <script>
 export default {
   namae: "MainPage",
+  data() {
+    return {
+      projects: [
+        {
+          id: 1,
+          image: require("@/assets/projectimgs/e1.png"),
+          name: "E-Commerce",
+          description:
+            "This is frontend of an e-commerce website. This work done by me from scratch. A work of my project",
+          link: "https://ecomwebbyarslanmatloob.netlify.app/",
+        },
+        {
+          id: 2,
+          image: require("@/assets/projectimgs/slack1.png"),
+          name: "Slack (Clone)",
+          description:
+            "This is frontend of a well-known website named Slack. For best practice of front-end development.",
+          link: "https://slackbyarslanmatloob.netlify.app/",
+        },
+        {
+          id: 3,
+          image: require("@/assets/projectimgs/t1.png"),
+          name: "Tic Tac Toe Pro",
+          description:
+            "This is a small website complete game with new features and functions. A practice of Javascript.",
+          link: "https://tictactoegamebyarslanmatloob.netlify.app/",
+        },
+        {
+          id: 4,
+          image: require("@/assets/projectimgs/w1.png"),
+          name: "Web dazzels",
+          description:
+            "This is my own project and to be lounched officially soon. This is the master piece of my work.",
+          link: "https://webdazzels.netlify.app/",
+        },
+      ],
+    };
+  },
 };
 </script>
 <style scoped>
@@ -153,10 +217,6 @@ export default {
 .image img:hover {
   transform: translateY(0px);
 }
-*i {
-  background-color: #694966;
-  color: #fdefef;
-}
 .main {
   background-color: #fdefef;
   padding-left: 30px;
@@ -173,15 +233,28 @@ export default {
 .borders {
   position: relative;
   border-left: 3px solid #74526c;
+  border-bottom: 2px solid #74526c;
 }
 .siders {
   border-radius: 50%;
-  top: 50px;
-  left: -18px;
+  top: 44px;
+  left: -1.4dvw;
+  height: 2.6dvw;
+  width: 2.6dvw;
+  padding: 2px 6px;
   position: absolute;
   z-index: 1;
-  color: #fce1e1;
   background-color: #74526c;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.fa-cogs,
+.fa-folder-open,
+.fa-id-badge,
+.fa-book {
+  font-size: 1.2dvw;
+  color: #fce2f9;
 }
 .divs h1,
 h2,
@@ -209,5 +282,71 @@ h6 {
 }
 .divs h6 {
   font-size: 1.2vw;
+}
+.projects {
+  padding: 0;
+  margin: 0;
+}
+.main-pro {
+  padding: 0;
+  margin: 0;
+}
+
+.proj-card {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  height: fit-content;
+  border-radius: 8px;
+  padding: 10px;
+  box-shadow: 1px 1px 10px 1px #74526c;
+  border: 1px solid #74526c;
+  transition: all 0.3s ease-in-out;
+  cursor: pointer;
+  margin: 20px 50px;
+}
+.proj-card:hover {
+  transform: scale(1.008);
+}
+.images {
+  width: 100%;
+  height: 50%;
+}
+.images img {
+  width: 100%;
+  height: 120px;
+  border-radius: 6px;
+  transition: all 0.4s ease-in-out;
+}
+.images img:hover {
+  transform: scale(1.08);
+}
+
+.info {
+  text-align: center;
+}
+.info p {
+  font-size: 1dvw;
+}
+.info h4 {
+  font-size: 1.8dvw;
+}
+.view-btn {
+  border: 1px solid #74526c;
+  border-radius: 6px;
+  font-size: 1dvw;
+  padding: 4px 16px;
+  color: #694966;
+  transition: all 0.3s ease-in-out;
+}
+.view-btn:hover {
+  transform: scale(1.01);
+  box-shadow: 1px 1px 8px 1px #74526c;
+}
+@media screen and (max-width: 678px) {
+  .proj-card {
+    padding: 10px;
+    margin: 8px 14px;
+  }
 }
 </style>
